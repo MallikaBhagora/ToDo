226e9497acaf83168d6dce001330eb5151a51a9e
MB <appa221b@gmail.com>
2021-01-25 16:02:59 +0530

    basic functionality to add title

--- app.js
+++ app.js
@@ -2,48 +2,43 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var titles = ["add","delete","update"];
+//var descriptions =[];
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("ERROR!!! currentDay is equal to " + currentDay);
-  }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-    kindDay: day
+    kindDay: day,
+    newTitles: titles,
+    //  newDescriptions:descriptions
   });
 });
 
+app.post("/", function(req, res) {
+  var title = req.body.title;
+  //  var description = req.body.description;
+
+  titles.push(title);
+  //  descriptions.push(description);
+
+  res.redirect("/");
+});
 
 
-
-app.llisten(3000, function(req, res) {
+app.listen(3000, function(req, res) {
   console.log("Server started at port 3000");
 });
